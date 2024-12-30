5ca229ad73646085e0055d1d55104ebeef1a4d12
IHGitter <111471561+IHGitter@users.noreply.github.com>
2024-12-30 20:57:43 +0900

    [add]TOPコンポーネントの追加とルーティングの追加

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'top',
+        loadComponent: () => import('./top/top.component').then(m => m.TopComponent)
+    }
+];
+
+export default routes;

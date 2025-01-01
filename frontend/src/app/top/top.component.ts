dadd35dfc5f37bd46d351d61a6a133c8c2f1784d
IHGitter <111471561+IHGitter@users.noreply.github.com>
2025-01-01 15:26:08 +0900

    [update]Update Angualr version to v19

--- frontend/src/app/top/top.component.ts
+++ frontend/src/app/top/top.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-top',
-  standalone: true,
-  imports: [],
-  templateUrl: './top.component.html',
-  styleUrl: './top.component.css'
+    selector: 'app-top',
+    imports: [],
+    templateUrl: './top.component.html',
+    styleUrl: './top.component.css'
 })
 export class TopComponent {
 
